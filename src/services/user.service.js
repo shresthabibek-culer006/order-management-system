d00c138cfc5f5eadf40ba457184df51fc08394e2
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-15 08:50:45 +0545

    add read and update user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,16 +2,31 @@ const User = require("./models/user.models");
 
 const createUser = async (req, res) => {
   const { name, email, password, role } = req.body
-
-  const user = new User({
-    name,
-    email,
-    password,
-    role
-  })
-
+  const user = new User({ name, email, password, role })
   await user.save()
   res.status(201).json({ message: "User created successfully", user })
 }
 
-module.exports = { createUser }
+const getAllUsers = async (req, res) => {
+  const users = await User.find()
+  res.status(200).json(users)
+}
+
+const getUserById = async (req, res) => {
+  const { id } = req.params
+  const user = await User.findById(id)
+  res.status(200).json(user)
+}
+
+const updateUser = async (req, res) => {
+  const { id } = req.params
+  const { name, email, password, role } = req.body
+  const user = await User.findByIdAndUpdate(
+    id,
+    { name, email, password, role },
+    { new: true }
+  )
+  res.status(200).json({ message: "User updated successfully", user })
+}
+
+module.exports = { createUser, getAllUsers, getUserById, updateUser }
